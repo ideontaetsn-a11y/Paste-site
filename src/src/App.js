7e4ae6a9d2cad59f96ba0a58ac10c65325348ed8
ideontaetsn-a11y <ideontaetsn@gmail.com>
2026-01-18 12:11:29 -0500

    Delete src/src directory

--- src/src/App.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ background: "black", minHeight: "100vh", color: "white", padding: 40 }}>
-      <h1 style={{ color: "#22c55e" }}>PASTE</h1>
-      <p>Vercel-ready Vite + React setup</p>
-    </div>
-  );
-}
